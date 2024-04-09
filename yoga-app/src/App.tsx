0d3118669197c3809f82232e47f0c5315329bd96
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-04-09 16:48:32 +0200

    add icons, fonts and colors

--- yoga-app/src/App.tsx
+++ yoga-app/src/App.tsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import "./scss/main.scss";
 
 function App() {
-
-
   return (
-    <>   
+    <>
+      <div className="main-div">
+        <h1 className="heading">Welcome to this Yoga website!</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
