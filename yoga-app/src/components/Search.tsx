6c29e36b26293fc57e4daffe342263946a9e54f3
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-04-11 16:41:35 +0200

    add form logic

--- yoga-app/src/components/Search.tsx
+++ yoga-app/src/components/Search.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
 
 export const Search = () => {
+  const [inputValBefore, setInputVal] = useState("");
+  const fetchOnSubmit: FormEventHandler<HTMLFormElement>  = () => {
+
+  } 
   return (
     <>
       <div className="search-container">
-        <input type="search" placeholder="Search" name="search" className="search" />
+        <form onSubmit={fetchOnSubmit}>
+          <input value={inputValBefore}type="search" placeholder="Search" name="search" onChange={(e) => setInputVal(e.target.value)} className="search" />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
